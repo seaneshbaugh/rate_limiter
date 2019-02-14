d9ead8e583293c018e83105e142ee0354a56e669
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-14 12:37:40 -0600

    Use alias instead of alias_method.

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -14,7 +14,7 @@ module RateLimiter
       yield @config if block_given?
       @config
     end
-    alias_method :configure, :config
+    alias configure config
 
     def enabled=(value)
       ::RateLimiter.config.enabled = value
