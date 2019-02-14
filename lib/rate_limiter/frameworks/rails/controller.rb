ba2410caa55fa8da9f9c190e5b4ad2825b08a709
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-14 12:59:54 -0600

    Added some comments.

--- lib/rate_limiter/frameworks/rails/controller.rb
+++ lib/rate_limiter/frameworks/rails/controller.rb
@@ -2,6 +2,8 @@
 
 module RateLimiter
   module Rails
+    # Extensions for Rails controllers. Allows for rate limiting to be turned on
+    # or off without disabling it on the model.
     module Controller
       def self.included(base)
         base.before_action(
@@ -12,22 +14,38 @@ module RateLimiter
 
       protected
 
+      # TODO: Reconsider whether or not this is even needed.
       def user_for_rate_limiter
         current_user
       rescue NoMethodError
         nil
       end
 
+      # Returns `true` or `false` depending on whether rate imiting should be
+      # active for the current request for all models.
+      #
+      # Override this method in your controller to turn rate limiting on or off.
+      #
+      # ```
+      # def rate_limiter_enabled_for_controller
+      #   # It is recommended that you always call `super` here unless simply
+      #   # returning `false`.
+      #   super && !user_for_rate_limiter.has_role?(:admin)
+      # end
+      # ```
       def rate_limiter_enabled_for_controller
         ::RateLimiter.enabled?
       end
 
       private
 
+      # Tells RateLimiter whether rate limiting should be enabled for the
+      # current request.
       def set_rate_limiter_enabled_for_controller
         ::RateLimiter.request.enabled = rate_limiter_enabled_for_controller
       end
 
+      # TODO: Reconsider whether or not this is even needed.
       def set_rate_limiter_source
         ::RateLimiter.request.source = user_for_rate_limiter if rate_limiter_enabled_for_controller
       end
