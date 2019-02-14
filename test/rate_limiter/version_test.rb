89711cbb90664673b39d8740b7f7ae7e24543d26
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-02-14 13:26:25 -0600

    Added test for version number.

---
+++ test/rate_limiter/version_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module RateLimiter
+  class VersionTest < ActiveSupport::TestCase
+    # See https://github.com/semver/semver/pull/460
+    SEMVER_REGEX = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
+
+    it 'returns a SemVer string' do
+      assert(::RateLimiter::VERSION.match(SEMVER_REGEX))
+    end
+  end
+end
