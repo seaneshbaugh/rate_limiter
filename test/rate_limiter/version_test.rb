c4313c865bc23e8b2e3098f8b4dd9e3ebec9b2c5
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-07-18 14:54:27 -0500

    Freeze constant.

--- test/rate_limiter/version_test.rb
+++ test/rate_limiter/version_test.rb
@@ -3,7 +3,7 @@
 module RateLimiter
   class VersionTest < ActiveSupport::TestCase
     # See https://github.com/semver/semver/pull/460
-    SEMVER_REGEX = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
+    SEMVER_REGEX = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/.freeze
 
     it 'returns a SemVer string' do
       assert(::RateLimiter::VERSION.match(SEMVER_REGEX))
