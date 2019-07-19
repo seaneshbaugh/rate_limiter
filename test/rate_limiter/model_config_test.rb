1faf08ba16c59e4520130c815320f3e08d363625
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-07-19 18:10:36 -0500

    Added some more tests.

--- test/rate_limiter/model_config_test.rb
+++ test/rate_limiter/model_config_test.rb
@@ -4,6 +4,68 @@ require 'test_helper'
 
 module RateLimiter
   class ModelConfigTest < ActiveSupport::TestCase
+    describe 'rate_limit' do
+      describe 'on:' do
+        it 'allows field for the scope of rate limiting to be set' do
+          dummy_model = Class.new do
+            include ActiveModel::Model
+            include ActiveModel::Validations
+            include ::RateLimiter::Model
 
+            attr_accessor :name
+
+            rate_limit on: :name
+          end
+
+          assert_equal :name, dummy_model.rate_limiter_options[:on]
+        end
+      end
+
+      describe 'interval:' do
+        it 'allows the time interval for rate limiting be set' do
+          dummy_model = Class.new do
+            include ActiveModel::Model
+            include ActiveModel::Validations
+            include ::RateLimiter::Model
+
+            attr_accessor :ip_address
+
+            rate_limit interval: 2.minutes
+          end
+
+          assert_equal 120, dummy_model.rate_limiter_options[:interval]
+        end
+      end
+
+      describe 'if:' do
+        it 'allows the if condition for rate limiting to be set' do
+          dummy_model = Class.new do
+            include ActiveModel::Model
+            include ActiveModel::Validations
+            include ::RateLimiter::Model
+
+            attr_accessor :ip_address
+
+            rate_limit if: -> (record) { true }
+          end
+
+          assert dummy_model.rate_limiter_options[:if]
+        end
+      end
+
+      it 'allows the unless condition for rate limiting to be set' do
+        dummy_model = Class.new do
+          include ActiveModel::Model
+          include ActiveModel::Validations
+          include ::RateLimiter::Model
+
+          attr_accessor :ip_address
+
+          rate_limit unless: -> (record) { false }
+        end
+
+        assert dummy_model.rate_limiter_options[:unless]
+      end
+    end
   end
 end
--- test/rate_limiter/rate_limiter_test.rb
+++ test/rate_limiter/rate_limiter_test.rb
@@ -4,6 +4,62 @@ require 'test_helper'
 
 module RateLimiter
   class RateLimiterTest < ActiveSupport::TestCase
+    describe '#exceeded?' do
+      let(:user) { User.new(email: 'test@test.com') }
+      let(:message1) { Message.new(user: user, subject: 'test 1', body: 'test', ip_address: '127.0.0.1') }
+      let(:message2) { Message.new(user: user, subject: 'test 2', body: 'test', ip_address: '127.0.0.1') }
 
+      context 'when enabled? and rate_limit? and others_for_rate_limiting.present?' do
+        it 'returns true' do
+          message1.save
+
+          assert message1.persisted?
+
+          rate_limiter = ::RateLimiter::RateLimiter.new(message2)
+
+          assert rate_limiter.exceeded?
+        end
+      end
+
+      context 'when enabled? is false' do
+        after do
+          ::RateLimiter.config.enabled = true
+        end
+
+        it 'returns false' do
+          ::RateLimiter.config.enabled = false
+
+          message1.save
+
+          assert message1.persisted?
+
+          rate_limiter = ::RateLimiter::RateLimiter.new(message2)
+
+          refute rate_limiter.exceeded?
+        end
+      end
+
+      context 'when rate_limit? is false' do
+        it 'returns false' do
+          if_condition = -> (_record) { false }
+
+          message1.save
+
+          assert message1.persisted?
+
+          rate_limiter = ::RateLimiter::RateLimiter.new(message2, if_condition: if_condition)
+
+          refute rate_limiter.exceeded?
+        end
+      end
+
+      context 'when others_for_rate_limiting? is empty' do
+        it 'returns false' do
+          rate_limiter = ::RateLimiter::RateLimiter.new(message1)
+
+          refute rate_limiter.exceeded?
+        end
+      end
+    end
   end
 end
