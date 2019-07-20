07f61e69cf46545a6f2b0ee7ade3a084c8e14ca9
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-07-20 17:43:15 -0500

    Added some comments.

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -7,8 +7,12 @@ require 'rate_limiter/config'
 require 'rate_limiter/model'
 require 'rate_limiter/request'
 
+# Extend your ActiveRecord models with the ability to limit the rate at which
+# they are saved.
 module RateLimiter
   class << self
+    # Return the RateLimiter singleton configuration object. This is for all
+    # threads.
     def config
       @config ||= ::RateLimiter::Config.instance
       yield @config if block_given?
@@ -16,14 +20,22 @@ module RateLimiter
     end
     alias configure config
 
+    # Switches RateLimiter on or off, for all threads.
     def enabled=(value)
       ::RateLimiter.config.enabled = value
     end
 
+    # Returns `true` if RateLimiter is on, `false if it is off. This is for all
+    # threads.
     def enabled?
       ::RateLimiter.config.enabled
     end
 
+    # Gets the options local to the current request.
+    #
+    # If given a block the options passed in are set, the block is executed,
+    # previous options are restored, and the return value of the block is
+    # returned.
     def request(options = nil, &block)
       if options.nil? && !block_given?
         Request
@@ -34,10 +46,14 @@ module RateLimiter
   end
 end
 
+# See https://guides.rubyonrails.org/engines.html#what-are-on-load-hooks-questionmark
+# for more information on `on_load` hooks.
 ActiveSupport.on_load(:active_record) do
   include RateLimiter::Model
 end
 
+# Load Rails controller extensions if RateLimiter is being used in a Rails
+# application.
 if defined?(::Rails)
   if defined?(::Rails.application)
     require 'rate_limiter/frameworks/rails'
